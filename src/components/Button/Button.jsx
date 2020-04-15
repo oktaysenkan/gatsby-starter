93eb3e73e980cd53a649955e5fadecfa50c78c31
Oktay Şenkan <oktay.senkan@gmail.com>
2020-04-15 21:49:09 +0300

    feat: 🎸 ESLint, prettier are configured

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = ({ children }) => <button type="button">{children}</button>;
+
+Button.propTypes = {
+  children: PropTypes.string.isRequired,
+};
+
+export default Button;
---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Button from '../components/Button';
+
+export default () => (
+  <div>
+    <Button>Test</Button>
+  </div>
+);
